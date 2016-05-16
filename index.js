e55081cfeeb2abdd62793a4ea90866a563638dda
Sergio Arbeo <serabe@gmail.com>
2016-05-16 23:42:20 +0200

    Add actioncable

--- index.js
+++ index.js
@@ -2,5 +2,23 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-deadpool'
+  name: 'ember-deadpool',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    if (app && typeof app.import === 'function') {
+      app.import('vendor/actioncable.js');
+    }
+  },
+
+  _findApp: function(hostApp) {
+    var app = this.app || hostApp;
+
+    while (app.app) {
+      app = app.app;
+    }
+
+    return app;
+  }
 };
